533f63760e00e62b1ef3eb1ef9b59b116f4d8582
Daniil Kulchenko <daniil@kulchenko.com>
2023-06-05 17:54:00 -0700

    make dead and liveview examples look the same

--- lib/navbug_web/live.ex
+++ lib/navbug_web/live.ex
@@ -7,6 +7,10 @@ defmodule NavbugWeb.Live do
 
   def render(assigns) do
     ~H"""
+    <h1>Liveview version (broken)</h1>
+    <a href="/nonlive">Go to deadview version</a>
+    <br /><br />
+
     <div phx-hook="Editor" id="editor-live" phx-update="ignore"></div>
     """
   end
